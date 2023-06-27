e39cc856a401f0a7dea60e423234398a8e08f163
omar shalaby <97606456+OmarShalaby9@users.noreply.github.com>
2023-06-27 21:21:38 +0300

    init

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { TodoWarpper } from './componant/TodoWarpper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWarpper/>
+    </div>
+  );
+}
+
+export default App;
